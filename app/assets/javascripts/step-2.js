18902a9a6f660c7333ed9c51ac998f4c9bb7e3c2
Paul Carnibella <paulcarnibella@gmail.com>
2017-03-27 19:50:51 +1100

    Added global JS service

--- app/assets/javascripts/step-2.js
+++ app/assets/javascripts/step-2.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  Toggler().init({
+  MSP.Toggler().init({
     btnSelector: '.btn-call-type',
     dataKey: 'data-call-type',
     localStorageKey: 'call-type'
   })
 
-  Toggler().init({
+  MSP.Toggler().init({
     btnSelector: '.btn-campaign',
     dataKey: 'data-campaign',
     localStorageKey: 'campaign'
