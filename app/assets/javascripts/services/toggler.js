506f8debcbf81c8d3f3da2ad78f6778a4993d3d4
Paul Carnibella <paulcarnibella@gmail.com>
2017-05-22 18:19:03 +1000

    Fixing backtick

--- app/assets/javascripts/services/toggler.js
+++ app/assets/javascripts/services/toggler.js
@@ -126,7 +126,7 @@ MSP.Toggler = function() {
 
   return {
     init: function(options) {
-      togglesSelector = `[data-${options.key}]`
+      togglesSelector = '[data-' + options.key + ']'
       dataKey = 'data-' + options.key
       localStorageKey = options.key
       count = options.count || 1
